6ad6997a4f3c86b5dbb63d1d5b820068c8cdb659
Danielrn7 <31738492+Danielrn7@users.noreply.github.com>
2017-11-24 14:11:04 -0600

    Initial Commit

---
+++ PizzaAppleWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PizzaAppleWatch
+//
+//  Created by Daniel Ramirez on 24/11/17.
+//  Copyright © 2017 Daniel Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
